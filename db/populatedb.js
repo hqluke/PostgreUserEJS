3d8dff32b03a4257872a00421d03133e9349f21b
hqluke <hqluke@users.noreply.github.com>
2026-01-21 17:50:06 -0700

    Updated for deployment

---
+++ db/populatedb.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+require('dotenv').config(); // Load .env for local development
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS usernames (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR ( 255 )
+);
+
+INSERT INTO usernames (username) 
+VALUES
+  ('Bryan'),
+  ('Odin'),
+  ('Damon')
+ON CONFLICT DO NOTHING;
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
